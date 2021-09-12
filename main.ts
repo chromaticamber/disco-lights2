80e92631098c6c9e796c2f898084fa2aef22fa31
Amber <86908677+chromaticamber@users.noreply.github.com>
2021-09-12 11:11:18 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     if (min < 192) {
-        min += 1
+        min += -1
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (min < 192) {
-        min += -1
+        min += 1
     }
 })
 let sound_level = 0
@@ -21,7 +21,7 @@ basic.showLeds(`
 basic.forever(function () {
     sound_level = input.soundLevel()
     if (sound_level < min) {
-    	
+        sound_level = min
     }
-    led.setBrightness(255)
+    led.setBrightness(Math.map(sound_level, min, 255, 0, 255))
 })
